05a79e20dd21d904caaf3f12d9ee3ac85a7a72de
doramonz <32805709+doramonz@users.noreply.github.com>
2024-02-22 15:36:34 +0900

    Task 수정

--- src/main/java/com/nhnacademy/doorayProject/entity/Task.java
+++ src/main/java/com/nhnacademy/doorayProject/entity/Task.java
@@ -19,10 +19,15 @@ public class Task {
     @Column(name = "task_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer taskId;
+    @Column(name = "project_id")
     private Integer projectId;
+    @Column(name = "user_id")
     private String userId;
+    @Column(name = "milestone_id")
     private Integer mileStoneId;
+    @Column(name = "tag_id")
     private String taskTitle;
+    @Column(name = "task_content")
     private String taskContent;
     @Enumerated(EnumType.STRING)
     private Status status;
